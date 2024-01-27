4bec49909d7309b58b12f19befc21321f1f28252
Nayan Gupta <kanumgupta32@gmail.com>
2024-01-27 15:48:35 +0530

    Sidebar updated

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import './Sidebar.css';
 //import {Logo} from '../Constants.js';
 import Logo from "../image/logo.png";
 import { SidebarData } from "../Data/data";
 // import {UilEstate} from '@iconscout/react-unicons';
-
+import {UilSignOutAlt} from '@iconscout/react-unicons';
 export const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
   return (
     <div className="sidebar">
       <div className="logo">
@@ -18,13 +19,20 @@ export const Sidebar = () => {
         {
           SidebarData.map((item, index) => {
             return (
-              <div className="menuItem">
+              <div 
+                className={(selected === index)?"menuItem active" : "menuItem"}
+                key={index}
+                onClick={() => setSelected(index)}
+              >
                 <item.icon/>
                 <span>{item.heading}</span>
               </div>
             );
           })   
         }
+        <div className="menuItem">
+          <UilSignOutAlt/>
+        </div>
       </div>
     </div>
   )
